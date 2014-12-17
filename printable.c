52b72162f296d2d8c9e6043822c503477175ab7c
ptrcarta <ptrcarta@gmail.com>
2014-12-17 13:17:03 +0100

    moved the declaration of the function fun array in the global scope

--- printable.c
+++ printable.c
@@ -2,31 +2,34 @@
 #include <stdio.h>
 #include <ctype.h>
 #define VECT_LEN 20
-int main() {
-    
-    int (*fun[])(int) = {
-        isprint,
-        isalnum,
-        isalpha,
-        isascii,
-        isblank,
-        iscntrl,
-        isdigit,
-        isgraph,
-        islower,
-        isupper,
-        ispunct,
-        isspace,
-        isxdigit,
-    };
+/* This is the array of available functions, each is associated with a 
+ * command line option. When adding functions do it at the end of the 
+ * array, so not to break the already defined command line arguments */
+int (*fun[])(int) = {
+    isprint,
+    isalnum,
+    isalpha,
+    isascii,
+    isblank,
+    iscntrl,
+    isdigit,
+    isgraph,
+    islower,
+    isupper,
+    ispunct,
+    isspace,
+    isxdigit,
+};
+
+int main(int argc, char ** argv) {
+    /* this array gets populated with the selected functions */
     int (*function_vector[VECT_LEN])(int);
-   
-
-
 
     int ch;
+    /* read characters on at a time. possibly it could be made more efficient
+     * by buffering */
     while ((ch = getchar()) != EOF) {
-        if (fun(ch)) {
+        if (execute_loop(ch, function_vector)) {
             putchar(ch);
         }
     }
@@ -34,7 +37,7 @@ int main() {
     return 0;
 }
 
-int execute_loop(int character, int * function_vector[]) {
+int execute_loop(int character, int * function_vector[](*)) {
     int result = 0;
     for(int i = 0; function_vector[i] != NULL; i++) {
         result = function_vector[i](character);
